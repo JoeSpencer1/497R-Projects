84ab2c903194593370e2f6e037dbe7cfa3cdfa38
JoeSpencer1 <108445563+JoeSpencer1@users.noreply.github.com>
2022-11-06 00:56:01 -0600

    I'm turning in for the night. The code is basically done besides that I need to figure out how to calculate the torque and bending moment of a rotor. I will finish the report after that.

--- Rotor Design/Rotor_Design.jl
+++ Rotor Design/Rotor_Design.jl
@@ -8,17 +8,19 @@ ready to be submitted.
 
 include("Rotor_Functions.jl")
 
-rread = readdlm("Rotor Design/Rotors/APC_10x7.txt") # Read rotor file.
-fread = "Rotor Design/Rotors/naca4412_1e6.dat" # Rename airfoil file.
-c0 = 10
-twist0 = 1
-v0 = 20.0
-rpm0 = 2000
+global rread = readdlm("Rotor Design/Rotors/APC_10x7.txt") # Read rotor file.
+global fread = "Rotor Design/Rotors/naca4412_1e6.dat" # Rename airfoil file.
+global M = moment() # Finds limit moment
+global T = torque() # Finds limit torque
+c0 = 10 # Initial estimate chord length
+twist0 = 1 # Initial estimate twist
+v0 = 20.0 # Initial estimate for airflow velocity
+rpm0 = 2000 # Initial estimate for rotational velocity
 
-optimize(c0, twist0, v0, rpm0, nb = 2)
+optimize(c0, twist0, v0, rpm0, nb = 2) # Optimize for 2 blades
 
-optimize(c, twist, v, rpm, nb = 3)
+optimize(c0, twist0, v0, rpm0, nb = 3) # Optimize for 3 blades
 
-optimize(c, twist, v, rpm, nb = 4)
+optimize(c0, twist0, v0, rpm0, nb = 4) # Optimize for 4 blades
 
-optimize(c, twist, v, rpm, nb = 8)
+optimize(c0, twist0, v0, rpm0, nb = 8) # Optimize for 8 blades
--- Rotor Design/Rotor_Functions.jl
+++ Rotor Design/Rotor_Functions.jl
@@ -6,10 +6,13 @@ longer have to pass them into functions. This had become a
 problem when they were passed into the optimizer function, since
 they cannot really be optimized.
 ---------------------------------------------------------------=#
-rread
-fread
 
-using CCBlade, FLOWMath, Xfoil, Plots, LaTeXStrings, DelimitedFiles, PointerArithmetic, SNOW, Setfield
+using Xfoil, CCBlade, SNOW, DelimitedFiles
+
+global rread # Rotor Data
+global fread # Airfoil Data
+global M # Original moment coefficient
+global T # Original torque
 
 """
    analysis(c, twist, v, rpm; nb = 3, d = 20, rho = 1.225, rfile = "Rotor Design/Rotors/APC_10x7.txt", ffile = "Rotor Design/Rotors/naca4412_1e6.dat")
@@ -23,9 +26,11 @@ Root function that calls other functions to analyze a rotor.
 - d - The rotor's diameter. Default 20 feet.
 - rhub - Ratio of the hub length to tip lentgh. Defulat 0.1.
 - rho - The air density. Default 1.225.
+# Outputs
+- obj - A variable for the output of the objective function. The program wants to minimize this.
 """
 function analysis(c, twist, v, rpm, nb, d, rhub, rho)
-    rtest = Rotortest(c, twist, v, rpm)
+    rtest = Rotortest(c, twist, v, rpm) # Create a rotor.
 
     # Rotor geometry
     d = d * 0.0254 # Diameter inches to meters
@@ -62,6 +67,18 @@ function analysis(c, twist, v, rpm, nb, d, rhub, rho)
 end
 
 """
+    moment
+Calculate the moment experienced by a rotor.
+# Arguments
+# Outputs
+- Mo - The total bending moment experienced by the rotor.
+"""
+function moment()
+    Mo = 1
+    return(Mo)
+end
+
+"""
     optimize(c, twist, v, rpm; nb = 3, d = 20, rhub = 0.1, rho = 1.225)
 This function uses the SNOW code to find the optimal properties of the rotor.
 # Arguments
@@ -73,28 +90,68 @@ This function uses the SNOW code to find the optimal properties of the rotor.
 - d - The rotor's diameter. Default 20 feet.
 - rhub - Ratio of the hub length to tip lentgh. Defulat 0.1.
 - rho - The air density. Default 1.225.
+# Outputs
+- xopt[1:4] - The optimal chord length, twist, velocity, and revolutions per minut for the rotor.
+- xopt[1] - The optimal chord length.
+- xopt[1] - The optimal twist.
+- xopt[1] - The optimal velocity.
+- xopt[1] - The optimal revolutions per minute.
 """
 function optimize(c, twist, v, rpm; nb = 3, d = 20, rhub = 0.1, rho = 1.225)
     x0 = [c; twist; v; rpm; nb; d; rhub; rho]  # starting point
-    ng = 3  # number of constraints
+    ng = 2 # number of constraints
     lx = [0.1, -45, 0, 0.1, nb, d, rhub, rho]  # lower bounds on x
     ux = [100.0, 45, 300, 10000, nb, d, rhub, rho]  # upper bounds on x
-    ng = 8  # number of constraints
-    lg = -Inf*ones(ng)  # lower bounds on g
-    ug = zeros(ng)  # upper bounds on g
+    lg = -Inf * ones(ng)  # lower bounds on g
+    ug = [1.1, 1.1] # upper bounds on g
     options = Options(solver=IPOPT())  # choosing IPOPT solver
 
+    # Optimize function with constraints defined above
     xopt, fopt, info = minimize(simple!, x0, ng, lx, ux, lg, ug, options)
 
-    println("xstar = ", xopt)
+    println("xstar = ", xopt[1:4]) # Print the found values (Only the variable ones). 
     println("fstar = ", fopt)
     println("info = ", info)
+
+    return(xopt[1:4])
+end
+
+"""
+    Rotortest
+this struct creates a new rotor with updated properties.
+# Members
+- c - Rotor's chord length, as a factor of the given lengths in the file.
+- twist - Rotor's twist. Twist is uniform across entire rotor.
+- v - The air velocity, in m/s.
+- rpm - The rotor's rotational velocity, in rpm.
+"""
+mutable struct Rotortest
+    c::Float64 # Chord length
+    twist::Float64 # Twist of entire rotor blade
+    v::Float64 # Wind velocity
+    rpm::Float64 # Rotational velocity
 end
 
 """
     simple!(g, x)
 This function is called from the optimize() function. It performs the rotor analysis over and over.
 # Arguments
+- g - Constraint functions used to restrain the moment coefficient and the torque coefficient.
+- g[1] - Constrains the bending moment coefficient below 110% original value. 
+- g[1] - Constrains the torque coefficient below 110% original value. 
+- x - This array contains the data contained in the Rotortest struct, in order.
+- x[1] - Chord length.
+- x[2] - Twist, in degrees.
+- x[3] - Air velocity, in meters per second.
+- x[4] - Rotational velocity, in rpm.
+- x[5] - The number of blades in the rotor. Default 3.
+- x[6] - The rotor's diameter. Default 20 feet.
+- x[7] - Ratio of the hub length to tip lentgh. Defulat 0.1.
+- x[8] - The air density. Default 1.225.
+# Outputs
+- g - Constraint functions used to restrain the moment coefficient and the torque coefficient.
+- g[1] - Constrains the bending moment coefficient below 110% original value. 
+- g[1] - Constrains the torque coefficient below 110% original value. 
 - x - This array contains the data contained in the Rotortest struct, in order.
 - x[1] - Chord length.
 - x[2] - Twist, in degrees.
@@ -106,27 +163,24 @@ This function is called from the optimize() function. It performs the rotor anal
 - x[8] - The air density. Default 1.225.
 """
 function simple!(g, x)
-    # constraints
-    # None
-
     # objective
-    f = analysis(x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8])
+    f = analysis(x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8]) # Calculate objective function
+
+    # constraints
+    g[1] = 1 # Bending moment coefficient constraint
+    g[2] = 1 # Torque coefficient constraint
 
     return f
 end
 
 """
-    Rotortest
-this struct creates a new rotor with updated properties.
+    torque
+Calculate the torque experienced by a rotor.
 # Arguments
-- c - Rotor's chord length, as a factor of the given lengths in the file.
-- twist - Rotor's twist. Twist is uniform across entire rotor.
-- v - The air velocity, in m/s.
-- rpm - The rotor's rotational velocity, in rpm.
+# Outputs
+- Tq - The torque experienced by the rotor.
 """
-mutable struct Rotortest
-    c::Float64
-    twist::Float64
-    v::Float64
-    rpm::Float64
+function torque()
+    Tq = 1
+    return(Tq)
 end
